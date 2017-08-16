abf4eef8b206afd9c5f037a1e7a875c065417667
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-08-16 12:58:00 +0200

    Create project Utils

---
+++ src/KMorcinek.TheCityCardGame.Utils/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMorcinek.TheCityCardGame.Utils
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            // ReSharper disable once PossibleMultipleEnumeration
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
