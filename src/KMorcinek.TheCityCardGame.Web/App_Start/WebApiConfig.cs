73ad9f729f76b5eb8f0ff97b032315c2c7dff630
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-08-16 16:46:32 +0200

    JSON for webapi

--- src/KMorcinek.TheCityCardGame.Web/App_Start/WebApiConfig.cs
+++ src/KMorcinek.TheCityCardGame.Web/App_Start/WebApiConfig.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Http;
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace KMorcinek.TheCityCardGame.Web
 {
@@ -10,6 +15,24 @@ namespace KMorcinek.TheCityCardGame.Web
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            // Set JSON format as default for WebAPI
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            // Serialize with camelCase formatter for JSON.
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            var settings = jsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            FormatAsIndentedInDebug(settings);
+        }
+
+        [Conditional("DEBUG")]
+        static void FormatAsIndentedInDebug(JsonSerializerSettings settings)
+        {
+            settings.Formatting = Formatting.Indented;
         }
     }
 }
