dd1a4c12c8c8626fdbc367ac3333789939ce9f9b
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-08-16 13:30:52 +0200

    ClientForWeb

---
+++ src/KMorcinek.TheCityCardGame.Console/DisconnectedClients/ClientForWeb.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMorcinek.TheCityCardGame.ConsoleUI.DisconnectedClients
+{
+    public class ClientForWeb
+    {
+        readonly DisconnectedGame _game = DisconnectedGame.Instance;
+        int _playerIndex;
+
+        public void Start()
+        {
+            int playerIndex = _game.Connect();
+
+            _playerIndex = playerIndex;
+
+            while (true)
+            {
+                TryMakeNextMove();
+
+                Task.Delay(TimeSpan.FromSeconds(1)).Wait();
+            }
+        }
+
+        void TryMakeNextMove()
+        {
+            IPlayer player = _game.GetState(_playerIndex);
+
+            if (player != null)
+            {
+                MakeNextMove(player);
+            }
+        }
+
+        void MakeNextMove(IPlayer player)
+        {
+            using (new ConsoleColorChanger(Game.Colors[_playerIndex]))
+            {
+                Game.ShowCards(player);
+
+                int cardIndexToPlay = Game.GetCardIndexToPlay();
+                int[] cardsToDiscard = Game.GetCardIndexesToDiscard();
+
+                Card playedCard = player.CardsInHand.ElementAt(cardIndexToPlay);
+
+                _game.PlayCard(_playerIndex, cardIndexToPlay, cardsToDiscard);
+
+                Game.ShowPlayedCard(playedCard);
+            }
+        }
+    }
+}
