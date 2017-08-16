116a5b5a940ab14e5a8955ac849fa8ae317e5633
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-08-16 13:54:45 +0200

    WIP GameServerWrapper

---
+++ src/KMorcinek.TheCityCardGame.Console/GameServerWrapper.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+
+namespace KMorcinek.TheCityCardGame.ConsoleUI
+{
+    public class GameServerWrapper : IGameServer
+    {
+        const string ServerUrl = "http://localhost:7820/api/game";
+
+        public int Connect()
+        {
+            var client = new RestClient(ServerUrl);
+            var request = new RestRequest("connect", Method.GET);
+
+            return client.Execute<int>(request).Data;
+        }
+
+        public IPlayer GetState(int playerIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PlayCard(int playerIndex, int cardIndexToPlay, int[] cardsToDiscard)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
