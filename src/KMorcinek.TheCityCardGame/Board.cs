da0ddb4464a3d807a2b4b2ace81af6546d6937d0
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-08-17 23:38:32 +0200

    logging

--- src/KMorcinek.TheCityCardGame/Board.cs
+++ src/KMorcinek.TheCityCardGame/Board.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using KMorcinek.TheCityCardGame.SharedDtos;
 using Serilog;
 
 namespace KMorcinek.TheCityCardGame
