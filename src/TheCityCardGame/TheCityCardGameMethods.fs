a062d0edac5a62f69f901982f1aba9bdb9ebaacf
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-09-04 16:39:37 +0200

    Nice renames

--- src/TheCityCardGame/TheCityCardGameMethods.fs
+++ src/TheCityCardGame/TheCityCardGameMethods.fs
@@ -12,10 +12,24 @@ let playCard (player: Player) cardToPlay (cardsToDiscard: CardEnum List) =
 
     if playedCard.Cost > 0 then invalidOp "Cannot discard the same card twice"
 
-    
+    let removeFirstCard bigList card =
+        let action (stateSoFar: Card List*CardEnum*bool) current =
+            let (items, cardToFind, isAlreadyRemoved) = stateSoFar
+
+            if isAlreadyRemoved = false &&  current.CardEnum = cardToFind
+                then (items, cardToFind, true)
+                else (current::items, cardToFind, isAlreadyRemoved)
+
+        bigList |> List.fold action ([], cardToPlay, false)
 
     // remove first occurence of ...
+    let (withoutPlayedCard, _, _) = (removeFirstCard player.CardsInHand cardToPlay)
 
+    let removeExactlyOne (bigLista: Card List) card =
+        let (withoutDiscardedCard, _, wasRemoved) = removeFirstCard player.CardsInHand cardToPlay
+
+        if wasRemoved = false then invalidOp "Cannot discard the same card twice"
+        else withoutDiscardedCard
 
     let player1 = { player with CardsInHand = [] }
     { player1 with PlayedCards = [playedCard] }
