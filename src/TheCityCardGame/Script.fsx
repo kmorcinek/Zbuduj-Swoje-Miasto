90f331898d37ac0b0ce707c03bdc09c0953479fe
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-09-04 14:27:01 +0200

    trying wip

--- src/TheCityCardGame/Script.fsx
+++ src/TheCityCardGame/Script.fsx
@@ -5,3 +5,36 @@
 open TheCityCardGame
 
 // Define your library scripting code here
+type NameAndSize= {Name:string;Size:int}
+ 
+let maxNameAndSize list = 
+    
+    let innerMaxNameAndSize initialValue rest = 
+        let action maxSoFar x = if maxSoFar.Size < x.Size then x else maxSoFar
+        rest |> List.fold action initialValue 
+
+    // handle empty lists
+    match list with
+    | [] -> 
+        None
+    | first::rest -> 
+        let max = innerMaxNameAndSize first rest
+        Some max
+
+  
+let maxNameAndSize2 list = 
+
+    let action2 (currentItem:int) (soFar: int List) (wasAlreadyRemoved: bool) =
+        match
+    
+    let innerMaxNameAndSize initialValue rest = 
+        let action maxSoFar x = if maxSoFar.Size < x.Size then x else maxSoFar
+        rest |> List.fold action initialValue 
+
+    // handle empty lists
+    match list with
+    | [] -> 
+        None
+    | first::rest -> 
+        let max = innerMaxNameAndSize first rest
+        Some max  
