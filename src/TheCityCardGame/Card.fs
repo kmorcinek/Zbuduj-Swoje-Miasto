0549ef7c567c5064fcc8b3cc309614d7bb91771e
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-09-04 13:49:41 +0200

    Add CardEnum

--- src/TheCityCardGame/Card.fs
+++ src/TheCityCardGame/Card.fs
@@ -1,6 +1,8 @@
 ﻿namespace TheCityCardGame
 
-type Card = {Cost:int; CashPoints:int; WinPoints:int }
+type CardEnum = Parking=0 | Architect=1 | House=2  
+
+type Card = {Cost:int; CashPoints:int; WinPoints:int; CardEnum:CardEnum }
 
 type Player = {CardsInHand: Card List; PlayedCards: Card List}
 
